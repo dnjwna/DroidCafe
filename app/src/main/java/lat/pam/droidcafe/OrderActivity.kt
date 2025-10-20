9fe32c8e299e0ff449fab30efebefbbd0a7d5b76
danujuwana <danujuwana05@gmail.com>
2025-10-20 15:06:13 +0700

    Initial commit

---
+++ app/src/main/java/lat/pam/droidcafe/OrderActivity.kt
@@ -0,0 +1,73 @@
+package lat.pam.droidcafe
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.RadioButton
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class OrderActivity : AppCompatActivity() {
+
+    private lateinit var citySpinner: Spinner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+
+        // Setup Spinner
+        setupCitySpinner()
+    }
+
+    private fun setupCitySpinner() {
+        citySpinner = findViewById(R.id.city_spinner)
+
+        // Daftar kota
+        val cities = arrayOf("Jakarta", "Kota Bandung", "Kota Cimahi", "Kabupaten Bandung")
+
+        // Buat adapter untuk spinner
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, cities)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Set adapter ke spinner
+        citySpinner.adapter = adapter
+
+        // Set listener untuk spinner
+        citySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedCity = cities[position]
+                displayToast("Kota dipilih: $selectedCity")
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Do nothing
+            }
+        }
+    }
+
+    fun onRadioButtonClicked(view: View) {
+        // Is the button now checked?
+        val checked = (view as RadioButton).isChecked
+        when (view.getId()) {
+            R.id.sameday -> if (checked) // Same day service
+                displayToast(getString(R.string.same_day_messenger_service))
+
+            R.id.nextday -> if (checked) // Next day delivery
+                displayToast(getString(R.string.next_day_ground_delivery))
+
+            R.id.pickup -> if (checked) // Pick up
+                displayToast(getString(R.string.pick_up))
+
+            else -> {}
+        }
+    }
+
+    private fun displayToast(message: String) {
+        Toast.makeText(
+            applicationContext, message,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+}
